174574c071f9256fa68b58bc5f0976ba362d268c
HeldyisAE <heaguero@estudiantec.cr>
2026-05-10 02:13:18 -0600

    Añade sección de new releases

--- src/Pages/lobby.jsx
+++ src/Pages/lobby.jsx
@@ -1,9 +1,26 @@
+import './Lobby.css'
+import NewReleases from '../components/NewReleases';
+
 function Lobby() {
   return (
-    <>
-      <h2>Content</h2>
-    </>
-  );
+    <div className="lobby-wrapper">
+      <div className="toplayout">
+        <NewReleases/>
+      </div>
+      <div className="collagenotes-wrapper">
+        <h1> adios </h1>
+      </div>
+      <div className="discover-wrapper">
+        <h1> pan </h1>
+      </div>
+      <div className="carousel-wrapper">
+        <h1> men </h1>
+      </div>
+      <div className="comments">
+        <h1> ola </h1>
+      </div>
+    </div>
+  ); 
 }
 
 export default Lobby;
